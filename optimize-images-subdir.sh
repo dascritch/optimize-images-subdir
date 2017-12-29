e7b7cd16d212127df5dc7fccf9a10dbdf12d081b
Xavier Mouton-Dubosc <xaviermd@gmail.com>
2017-12-29 07:00:33 +0100

    Using substitution string for backup image

--- optimize-images-subdir.sh
+++ optimize-images-subdir.sh
@@ -20,44 +20,44 @@ Parameters:
         -h, --help              Will show this message
         OPTIMIZABLE_DIR         Path to sub directory to scan. Mandatory
         BACKUP_DIR              Path to a backup sub-directory, where files
-                                will be copied before processing.                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                                                                  
-Needed executables :                                                                                                                                                                                                                                                              
-        — find                                                                                                                                                                                                                                                                    
-        — mozjpeg from mozilla , version > 3.2       https://github.com/mozilla/mozjpeg                                                                                                                                                                                           
-        — jpeg-recompress from jpeg-archive > 2.1.1  https://github.com/danielgtaylor/jpeg-archive                                                                                                                                                                                
-        — exiftool                                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                                                  
-HELP                                                                                                                                                                                                                                                                              
-)                                                                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                                                                  
-option=${1}                                                                                                                                                                                                                                                                       
-                                                                                                                                                                                                                                                                                  
-case $option in                                                                                                                                                                                                                                                                   
-        -h|--help)                                                                                                                                                                                                                                                                
-                echo "$HELP"                                                                                                                                                                                                                                                      
-                exit 0                                                                                                                                                                                                                                                            
-        ;;                                                                                                                                                                                                                                                                        
-esac                                                                                                                                                                                                                                                                              
-                                                                                                                                                                                                                                                                                  
-OPTIMIZABLE_DIR=$1                                                                                                                                                                                                                                                                
-BACKUP_DIR=$2                                                                                                                                                                                                                                                                     
-                                                                                                                                                                                                                                                                                  
-du -sh ${OPTIMIZABLE_DIR}                                                                                                                                                                                                                                                         
-                                                                                                                                                                                                                                                                                  
-function _check_dirs() {                                                                                                                                                                                                                                                          
-        if [ "" == "${OPTIMIZABLE_DIR}" ] ; then                                                                                                                                                                                                                                  
-                echo "Missing optimizable directory parameter"                                                                                                                                                                                                                    
-                exit -1                                                                                                                                                                                                                                                           
-        fi                                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                                  
-        if [ ! -d "${OPTIMIZABLE_DIR}" ] ; then                                                                                                                                                                                                                                   
-                echo "Inexisting optimizable directory"                                                                                                                                                                                                                           
-                exit -1                                                                                                                                                                                                                                                           
-        fi                                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                                  
-        if [ "" != "${BACKUP_DIR}" ] ; then                                                                                                                                                                                                                                       
-                if [ ! -d "${BACKUP_DIR}" ] ; then                                                                                                                                                                                                                                
+                                will be copied before processing.
+
+Needed executables :
+        — find
+        — mozjpeg from mozilla , version > 3.2       https://github.com/mozilla/mozjpeg
+        — jpeg-recompress from jpeg-archive > 2.1.1  https://github.com/danielgtaylor/jpeg-archive
+        — exiftool
+
+HELP
+)
+
+option=${1}
+
+case $option in
+        -h|--help)
+                echo "$HELP"
+                exit 0
+        ;;
+esac
+
+OPTIMIZABLE_DIR=$1
+BACKUP_DIR=$2
+
+du -sh ${OPTIMIZABLE_DIR}
+
+function _check_dirs() {
+        if [ "" == "${OPTIMIZABLE_DIR}" ] ; then
+                echo "Missing optimizable directory parameter"
+                exit -1
+        fi
+
+        if [ ! -d "${OPTIMIZABLE_DIR}" ] ; then
+                echo "Inexisting optimizable directory"
+                exit -1
+        fi
+
+        if [ "" != "${BACKUP_DIR}" ] ; then
+                if [ ! -d "${BACKUP_DIR}" ] ; then
                         echo "Inexisting backup directory parameter"
                         exit -1
                 fi
@@ -80,8 +80,9 @@ function _optimize_file() {
         # See https://guides.wp-bullet.com/batch-optimize-jpg-lossy-linux-command-line-with-jpeg-recompress/
 
         FILENAME=${1}
-        echo "— ${FILENAME}"
-        DIRNAME=$(dirname "${FILENAME}")
+        BACKUP_FILENAME=${FILENAME/$OPTIMIZABLE_DIR/$BACKUP_DIR}
+        echo "— ${FILENAME}   ${BACKUP_FILENAME}"
+        BACKUP_DIRNAME=$(dirname "${BACKUP_FILENAME}")
         # replace OPTIMIZABLE_DIR in DIRNAME with BACKUP_DIR
         # jpeg-recompress --quality ${PREFERED_JPEG_QUALITY} --min ${MINIMUM_JPEG_QUALITY} --method smallfry --accurate "${FILENAME}" "${FILENAME}"
         # exiftool -overwrite_original -all= "${FILENAME}"
@@ -96,4 +97,3 @@ done
 du -sh ${OPTIMIZABLE_DIR}
 
 touch ${PROCESSED_FLAG_FILE}
-
